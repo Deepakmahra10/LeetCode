c588a632c8c415911e548d94790a945de5cf28a9
Deepak Mahra <dmahra05@gmail.com>
2026-06-01 21:28:46 +0530

    LC-longest-happy-prefix [Runtime: N/A; Memory: N/A]

--- string-matching/longest-happy-prefix.cpp
+++ string-matching/longest-happy-prefix.cpp
@@ -2,12 +2,17 @@ class Solution {
 public:
     string longestPrefix(string s) {
         int n = s.size();
-
-        for(int i=n-1; i>0; i--){
-            if(s.substr(0, i) == s.substr(n-i, i)){
-                return s.substr(0, i);
+        vector<int> lps(n, 0);
+        int len =0;
+        for(int i=1; i<n; i++){
+            if(s[i] == s[len]){
+                len++;
+                lps[i] = len;
+            }else if(len != 0){
+                len = lps[len-1];
+                i--;
             }
         }
-        return "";
+        return s.substr(0, lps[n-1]);
     }
 };
