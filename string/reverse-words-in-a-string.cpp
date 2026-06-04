f1f3ce65d2bf2c013c51fe5e6f84838939c423f2
Deepak Mahra <dmahra05@gmail.com>
2026-06-04 20:25:25 +0530

    LC-reverse-words-in-a-string [Runtime: N/A; Memory: N/A]

--- string/reverse-words-in-a-string.cpp
+++ string/reverse-words-in-a-string.cpp
@@ -2,28 +2,21 @@ class Solution {
 public:
     string reverseWords(string s) {
         int n = s.size();
-
-        // 1. Remove extra spaces
         int i = 0, j = 0;
-
         while (j < n) {
-            while (j < n && s[j] == ' ') j++;
-
-            while (j < n && s[j] != ' ') {
+            while (j < n && s[j] == ' ')
+                j++;
+            while (j < n && s[j] != ' ')
                 s[i++] = s[j++];
-            }
-
-            while (j < n && s[j] == ' ') j++;
-
-            if (j < n) s[i++] = ' ';
+            while (j < n && s[j] == ' ')
+                j++;
+            if (j < n)
+                s[i++] = ' ';
         }
 
         s.resize(i);
-
-        // 2. Reverse whole string
         reverse(s.begin(), s.end());
 
-        // 3. Reverse each word
         int start = 0;
         for (int end = 0; end <= s.size(); end++) {
             if (end == s.size() || s[end] == ' ') {
@@ -31,7 +24,6 @@ public:
                 start = end + 1;
             }
         }
-
         return s;
     }
 };
