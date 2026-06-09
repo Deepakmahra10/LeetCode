61cd389ab6e4dfd787e289fe51e35ac2484804f8
Deepak Mahra <dmahra05@gmail.com>
2026-06-09 20:18:16 +0530

    Synchronize existing submission history (1/2)

--- array/check-if-array-is-sorted-and-rotated.cpp
+++ array/check-if-array-is-sorted-and-rotated.cpp
@@ -1,26 +1,12 @@
 class Solution {
 public:
-    bool isSorted(vector<int>& rotated){
-        int n = rotated.size();
-        for(int i=0; i<n; i++){
-            if(rotated[i] < rotated[i-1]){
-                return false;
-            }
-        }
-        return true;
-    }
-
     bool check(vector<int>& nums) {
         int n = nums.size();
-        fora(int i=0; i<n; i++){
-            vector<int> rotated;
-            for(int j=0; j<n; j++){
-                rotated.push_back(nums[(i+j)%n]);
-            }
-            if(isSorted(rotated)){
-                return true;
-            }
+        int cnt = 0;
+        for(int i=0; i<n; i++){
+            if(nums[i] > nums[(i+1)%n])
+                cnt += 1;
         }
-        return false;
+        return cnt<=1;
     }
 };
--- recursion/reverse-linked-list.cpp
+++ recursion/reverse-linked-list.cpp
@@ -11,18 +11,12 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        stack<int> st;
-        ListNode* temp = head;
-        while(temp){
-            st.push(temp->val);
-            temp = temp->next;
-        }
-        temp = head;
-        while(temp){
-            temp->val = st.top();
-            st.pop();
-            temp = temp->next;
-        }
-        return head;
+        if(head == NULL || head->next == NULL)
+            return head;
+        ListNode* newNode = reverseList(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        
+        return newNode;
     }
 };
