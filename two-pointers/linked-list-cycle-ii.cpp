103dcae286cecc9a472dd5ee5c2ed31623c17dca
Deepak Mahra <dmahra05@gmail.com>
2026-07-02 20:33:18 +0530

    LC-linked-list-cycle-ii [Runtime: N/A; Memory: N/A]

--- two-pointers/linked-list-cycle-ii.cpp
+++ two-pointers/linked-list-cycle-ii.cpp
@@ -8,14 +8,20 @@
  */
 class Solution {
 public:
-    ListNode *detectCycle(ListNode *head) {
-        unordered_set<ListNode*> visited;
-        ListNode* temp = head;
-        while(temp!=NULL){
-            if(visited.find(temp) != visited.end())
-                return temp;
-            visited.insert(temp);
-            temp = temp->next;
+    ListNode* detectCycle(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) {
+                slow = head;
+                while (slow != fast) {
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow;
+            }
         }
         return NULL;
     }
