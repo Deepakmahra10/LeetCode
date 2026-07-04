5c88cbc59c9fdbefef037dc1ea67e969144130a1
Deepak Mahra <dmahra05@gmail.com>
2026-07-04 22:54:17 +0530

    LC-intersection-of-two-linked-lists [Runtime: N/A; Memory: N/A]

--- hash-table/intersection-of-two-linked-lists.cpp
+++ hash-table/intersection-of-two-linked-lists.cpp
@@ -8,17 +8,39 @@
  */
 class Solution {
 public:
+    int getDiff(ListNode* l1, ListNode* l2) {
+        int len1 = 0;
+        int len2 = 0;
+
+        while (l1 != NULL || l2 != NULL) {
+            if (l1 != NULL) {
+                len1++;
+                l1 = l1->next;
+            }
+            if (l2 != NULL) {
+                len2++;
+                l2 = l2->next;
+            }
+        }
+        return len1 - len2;
+    }
     ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
-        unordered_set<ListNode*> st;
+        int diff = getDiff(headA, headB);
+        if (diff < 0) {
+            while (diff++ != 0)
+                headB = headB->next;
+        } else {
+            while (diff-- != 0)
+                headA = headA->next;
+        }
 
         while (headA != NULL) {
-            st.insert(headA);
+            if (headA == headB)
+                return headA;
             headA = headA->next;
-        }
-        while (headB != NULL) {
-            if(st.find(headB)!= st.end()) return headB;
             headB = headB->next;
         }
-        return NULL;
+
+        return headA;
     }
 };
