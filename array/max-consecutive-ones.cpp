c927d80d1e9333634c8859232348e3a01534fc89
Deepak Mahra <dmahra05@gmail.com>
2026-07-04 13:58:31 +0530

    LC-max-consecutive-ones [Runtime: N/A; Memory: N/A]

--- array/max-consecutive-ones.cpp
+++ array/max-consecutive-ones.cpp
@@ -5,13 +5,13 @@ public:
         int maxOne = 0;
         int cnt = 0;
 
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 1) {
+        for (int num : nums) {
+            if (num == 1) {
                 cnt++;
             } else {
                 cnt = 0;
             }
-            maxOne = max(maxOne, cnt);
+            
         }
 
         return maxOne;
