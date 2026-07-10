a566c2d9ed6090d07218d63fc9cc154aa4c0d47d
Deepak Mahra <dmahra05@gmail.com>
2026-07-10 13:08:19 +0530

    LC-kids-with-the-greatest-number-of-candies [Runtime: N/A; Memory: N/A]

--- array/kids-with-the-greatest-number-of-candies.cpp
+++ array/kids-with-the-greatest-number-of-candies.cpp
@@ -2,17 +2,13 @@ class Solution {
 public:
     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
         int n = candies.size();
-        vector<bool> result(n, false);
+        vector<bool> result;
 
         int maxVal = *max_element(candies.begin(), candies.end());
-        for (int i = 0; i < n; i++) {
-            if (candies[i] + extraCandies >= maxVal) {
-                result[i] =true;
-            } else {
-                result[i] = false;
-            }
+        for (int candy : candies) {
+            result.push_back(candy + extraCandies >= maxVal);
         }
-        
+
         return result;
     }
 };
