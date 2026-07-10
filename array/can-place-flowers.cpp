6f45d66a76b8e59071c5584b0bab4526f64bdeee
Deepak Mahra <dmahra05@gmail.com>
2026-07-10 13:35:00 +0530

    LC-can-place-flowers [Runtime: N/A; Memory: N/A]

--- array/can-place-flowers.cpp
+++ array/can-place-flowers.cpp
@@ -7,8 +7,9 @@ public:
 
         for (int i = 0; i < m; i++) {
             if (temp[i] == 0) {
-                bool left = (i == 0 || flowerbed[i - 1] == 0);
-                bool right = (i == m - 1 || flowerbed[i + 1] == 0);
+
+                bool left = (i == 0 || temp[i - 1] == 0);
+                bool right = (i == m - 1 || temp[i + 1] == 0);
 
                 if (left && right) {
                     temp[i] = 1;
