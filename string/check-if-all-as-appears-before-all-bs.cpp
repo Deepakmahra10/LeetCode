339aef45db6ad799eb84b2fe9ce5b3360745b4e2
Deepak Mahra <dmahra05@gmail.com>
2026-07-11 14:07:02 +0530

    LC-check-if-all-as-appears-before-all-bs [Runtime: 0 ms; Memory: 8.2 MB]

--- string/check-if-all-as-appears-before-all-bs.cpp
+++ string/check-if-all-as-appears-before-all-bs.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool checkString(string s) {
         int n = s.size();
-        stack<char> st;
+        
         for(int i=0; i<n; i++){
             if(s[i] == 'b' && s[i+1] == 'a')
                 return false;
