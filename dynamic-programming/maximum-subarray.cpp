2f1870c71f8a56dc23ad6cf7fdcc773cad2a9bec
Deepak Mahra <dmahra05@gmail.com>
2026-07-14 21:03:00 +0530

    LC-maximum-subarray [Runtime: 60 ms; Memory: 71.9 MB]

--- dynamic-programming/maximum-subarray.cpp
+++ dynamic-programming/maximum-subarray.cpp
@@ -2,13 +2,26 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int n = nums.size();
-        int maxSum = INT_MIN;
-        int currSum = 0;
+        long long maxSum = LLONG_MIN;
+        long long currSum = 0;
+
+        int start = 0;
+        int ansStart = -1, ansEnd = -1;
+
         for (int i = 0; i < n; i++) {
+            if (currSum == 0)
+                start = i;
             currSum += nums[i];
-            maxSum = max(maxSum, currSum);
-            if (currSum < 0)
+            if (currSum > maxSum) {
+                maxSum = currSum;
+                ansStart = start;
+                ansEnd = i;
+            }
+
+            if (currSum < 0) {
                 currSum = 0;
+                start = i + 1;
+            }
         }
         return maxSum;
     }
