6871ac7e7adf0a3709da71a02cc527e71efb37fb
Deepak Mahra <dmahra05@gmail.com>
2026-07-14 20:44:33 +0530

    LC-maximum-subarray [Runtime: N/A; Memory: N/A]

--- array/maximum-subarray.cpp
+++ array/maximum-subarray.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int n = nums.size();
-        int maxSum = -1;
+        int maxSum = INT_MIN;
         for(int i=0; i<n; i++){
             int currSum = 0;
             for(int j=i; j<n; j++){
