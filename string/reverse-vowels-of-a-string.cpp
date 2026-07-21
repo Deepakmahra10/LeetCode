5280fd8c442eeabb94f6625a8314e075f6d5c22d
Deepak Mahra <dmahra05@gmail.com>
2026-07-21 23:09:12 +0530

    LC-reverse-vowels-of-a-string [Runtime: N/A; Memory: N/A]

--- string/reverse-vowels-of-a-string.cpp
+++ string/reverse-vowels-of-a-string.cpp
@@ -4,21 +4,19 @@ public:
         ch = tolower(ch);
         return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
     }
-
     string reverseVowels(string s) {
         int n = s.size();
-        string vowels;
-        for (char ch : s) {
-            if (isVowel(ch)) {
-                vowels += ch;
-            }
-        }
-        reverse(vowels.begin(), vowels.end());
-        int j = 0;
-        for (int i = 0; i < n; i++) {
-            if (isVowel(s[i])) {
-                s[i] = vowels[j++];
-            }
+        int i = 0;
+        int j = n - 1;
+
+        while (i < j) {
+            while(i<j && !isVowel(s[i]))
+                i++;
+            while(i<j && !isVowel(s[j]))
+                j--;
+            swap(s[i], s[j]);
+            i++;
+            j--;
         }
         return s;
     }
